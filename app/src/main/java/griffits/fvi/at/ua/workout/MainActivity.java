1136da88ac35feac509facb4d9d50be5200e3cc7
Victoria Frantsuh <eboko1@gmail.com>
2017-06-14 15:34:20 +0300

    adding WorkoutListFragment

--- app/src/main/java/griffits/fvi/at/ua/workout/MainActivity.java
+++ app/src/main/java/griffits/fvi/at/ua/workout/MainActivity.java
@@ -1,13 +1,19 @@
 package griffits.fvi.at.ua.workout;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+       // WorkoutDetailFragment wdf = (WorkoutDetailFragment)getFragmentManager().findFragmentById(R.id.detail_frag);
+        //wdf.setWorkOutId(1);
     }
 }
--- app/src/main/java/griffits/fvi/at/ua/workout/WorkoutDetailFragment.java
+++ app/src/main/java/griffits/fvi/at/ua/workout/WorkoutDetailFragment.java
@@ -13,17 +13,17 @@ import android.view.ViewGroup;
  */
 public class WorkoutDetailFragment extends Fragment {
 
+    private long workOutId;
 
-    public WorkoutDetailFragment() {
-        // Required empty public constructor
-    }
-
-
-    @Override
+      @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_workout_detail, container, false);
+    }
+
+    public void setWorkOutId(long id){
+        this.workOutId = id;
     }
 
 }
